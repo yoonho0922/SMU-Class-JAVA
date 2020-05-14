bbdfc0350a1386322b2d5bea119c0eeca0cb5a34
yoonho0922 <yoonho0922@naver.com>
2020-05-14 11:46:04 +0900

    add Ass08

--- Ass_08/src/Main.java
+++ Ass_08/src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args){
+        StudentDB db = new StudentDB();
+        Student student = new Student("김지우", 201911111, 4.01);
+
+        db.add(student);
+        db.add("이민준", 201922222, 3.85);
+        db.add("박서윤", 201933333, 3.90);
+
+
+
+        System.out.println("find by 김지우 - " + db.findBy("김지우"));
+        System.out.println("find by 201922222 - " + db.findBy(201922222));
+        System.out.println("find by 3.90 - " + db.findBy(3.90));
+
+        System.out.println("find by lower than 3.90 - " + db.findBy(StudentDB.FindGPAParam.LOWER_THAN, 3.95));
+        System.out.println("find by higher than 3.90 - " + db.findBy(StudentDB.FindGPAParam.HIGHER_THAN, 3.95));
+
+    }
 }
--- Ass_08/src/Student.java
+++ Ass_08/src/Student.java
@@ -1,2 +1,27 @@
 public class Student {
+    String name;
+    int number;
+    double gpa;
+
+    Student(String name, int number, double gpa){
+        this.name = name;
+        this.number = number;
+        this.gpa = gpa;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    int getNumber(){
+        return number;
+    }
+
+    double getGPA(){
+        return gpa;
+    }
+
+    public String toString(){
+        return "Name: "+name+", Number: "+number+", GPA: "+gpa;
+    }
 }
